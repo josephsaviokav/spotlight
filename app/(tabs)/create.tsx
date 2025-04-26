54d3e7df7e19a7cf369dcffc82ad9cf78fcc48ed
Joseph Savio <josephsaviokav@gmail.com>
2025-04-26 12:44:29 +0530

    26/02/2025

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,8 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { styles } from '../../styles/auth.style'
+import { useUser } from '@clerk/clerk-expo'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
 
-export default function create() {
+export default function createScreen() {
+  const router = useRouter()
+  const {user}=useUser()
+  const [caption,setcaption]=React.useState('');
+  const [selectedImage,setselectedImage]=useState<string | null>(null);
+  const [isSharing,setisSharing]=useState(false);
+
+
   return (
     <View style={styles.container}>
       <Text>create</Text>
