60f6ed63e803f0542d8fdca276c1b74dded9f620
Joseph Savio <josephsaviokav@gmail.com>
2025-05-01 09:41:19 +0530

    01/05/2025

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,12 +4,25 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Initial_layout from "@/components/initial_layout"
 import { Clerk } from '@clerk/clerk-expo';
 import ClerkAndConvexProvider from '@/providers/ClerkAndConvexProvider';
+import { SplashScreen } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
-  return (
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+ 
+        return (
    <ClerkAndConvexProvider>
     <SafeAreaProvider>
       <StatusBar style="light" /> {/* This makes status bar icons light (white) */}
-      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }} onLayout={onLayoutRootView}>
  
         <Initial_layout />
       </SafeAreaView>
